a0bf3784429ac89fd020c3b3c8294f9b12782ff4
ramrami-B <21801034@handong.ac.kr>
2023-08-30 03:17:36 +0900

    Fix: install dotenv-webpack to fix process.env error

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -36,6 +37,7 @@ module.exports = {
       template: './public/index.html',
       filename: 'index.html',
     }),
+    new Dotenv(),
   ],
   devServer: {
     static: path.join(__dirname, 'dist'),
