290a18bee0e1597585bde3d507fb6afaa13a6489
ramrami-B <21801034@handong.ac.kr>
2023-08-30 15:59:46 +0900

    Feat: store 생성, getIssues async thunk로 데이터 불러온 후 UI 생성(map 함수)

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,12 @@
 // routes.tsx
 import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import IssueListPage from './pages/IssueListPage';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" Component={Home} />
+      <Route path="/" Component={IssueListPage} />
     </Routes>
   );
 };
